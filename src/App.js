95dffcce4d7bc946a0a1b4bd63c48d09f0119380
Khaynes <khaynes@vailsys.com>
2024-02-22 13:57:22 -0600

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +25,31 @@ function App() {
 }
 
 export default App;
+*/
+
+import { Link, Route, Routes } from "react-router-dom"
+import {Home } from "./webpages/Home"
+import {InternPage} from "./webpages/InternPage"
+
+
+
+function App() {
+  return(
+    <>
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/internPage">Intern page</Link></li>
+        </ul>
+    </nav>
+  <Routes>
+    <Route path= "/" element={<Home />} />
+    <Route path= "/internPage" element={<InternPage />}/>
+  </Routes>
+  </>
+
+  );
+}
+
+export default App;
+
