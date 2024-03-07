de6beea1d6ff4eedb4a30235dd6137faf2c7b543
Khaynes <khaynes@vailsys.com>
2024-03-07 12:11:03 -0600

    Added MUI method to weatherapi

--- myfetch/src/App.js
+++ myfetch/src/App.js
@@ -46,6 +46,9 @@ export default App;
 */
 
 import React, { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import './App.css';
 
 const apiKey = '1b8e1b39d74b4fb4984174905240403';
@@ -54,6 +57,7 @@ const apiUrl = 'http://api.weatherapi.com/v1';
 async function fetchWeatherData(city) {
   try {
     const response = await fetch(`${apiUrl}/current.json?key=${apiKey}&q=${city}&aqi=no`);
+    console.log("response: ", response)
     return await response.json();
   } catch (error) {
     console.error('Error fetching weather data:', error);
@@ -61,16 +65,26 @@ async function fetchWeatherData(city) {
   }
 }
 
+async function fetchWeatherForcastData(city) {
+  try {
+    const response = await fetch(`${apiUrl}/forcast.json?key=${apiKey}&q=${city}&aqi=no`);
+    console.log("response: ", response)
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching weather forcast data:', error);
+    return null;
+  }
+}
+
+
 function App() {
   const [data, setData] = useState(null);
+  const [city, setCity] = useState("");
 
-  useEffect(() => {
-    async function fetchData() {
-      const weatherData = await fetchWeatherData('chicago');
-      setData(weatherData);
-    }
-    fetchData();
-  }, []);
+  async function fetchData(city) {
+    const weatherData = await fetchWeatherData(city);
+    setData(weatherData);
+  }
 
   console.log(data);
 
@@ -78,19 +92,82 @@ function App() {
     <div className="App">
       <div>
 
-        {data && data.current && (<>
-                                    <div> {data.location.name} </div>
-                                    <div>{data.location.region} </div>
-                                    <div> {data.location.country} </div>
-                                    <div> {data.location.localtime} </div>
-                                    <div> {data.current.temp_c} </div>
-                                    <div>{data.current.temp_f}</div>
-
-                                    </> )}
+        {data && data.current && 
+        (
+          <>
+              <div> {data.location.name} </div>
+              <div>{data.location.region} </div>
+              <div> {data.location.country} </div>
+              <div> {data.location.localtime} </div>
+              <div> {data.current.temp_c} </div>
+              <div>{data.current.temp_f}</div>
+                  </> 
+                )
+              }
         
       </div>
+      
+      <div>
+     <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '15ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField 
+        id="filled-basic"
+        label="City" 
+        variant="filled" 
+        onChange={(input) => {
+          setCity(input.target.value)
+
+        }}
+      />
+    </Box>
+
+     </div>
+      <Button variant="contained" onClick={() => {fetchData(city)}}>Submit </Button>
     </div>
+  );
+}
+export default App;
+
+//React-router-training 
+/*
+
+import { Link, Route, Routes } from "react-router-dom"
+import {Home } from "./webpages/Home"
+import {InternPage} from "./webpages/InternPage"
+import React, { useState, useEffect } from 'react';
+
+
+
+function App() {
+  return(
+    <>
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/internPage">Intern page</Link></li>
+        </ul>
+    </nav>
+
+
+    
+
+
+  <Routes>
+    <Route path= "/" element={<Home />} />
+    <Route path= "/internPage" element={<InternPage />}/>
+  </Routes>
+  </>
+
+
   );
 }
 
 export default App;
+
+*/
--- src/webpages/InternPage.js
+++
@@ -1,3 +0,0 @@
-export function InternPage(){
-    return <h1>Intern Page</h1>
-}
