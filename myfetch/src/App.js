31605dd4013a822951088d636592fea2d4ac389e
Khaynes <khaynes@vailsys.com>
2024-03-11 14:53:15 -0500

    Added CSS

--- myfetch/src/App.js
+++ myfetch/src/App.js
@@ -1,55 +1,10 @@
-/*
-
-import logo from './logo.svg';
-import './App.css';
-
-
-const apiKey = '1b8e1b39d74b4fb4984174905240403';
-const apiUrl = 'http://api.weatherapi.com/v1/';
-
-
-async function fetchWeatherData(city) {
-  try {
-      const response = await fetch(`${apiUrl}/current.json?key=${apiKey}&q=${city}&aqi=no`);
-      return await response.json();
-  } catch (error) {
-      console.error('Error fetching weather data:', error);
-      return null;
-  }
-}
-
-function App() {
-
-  const data = fetchWeatherData('chicago');
-  console.log(data)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
-
 import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import './App.css';
+
+
 
 const apiKey = '1b8e1b39d74b4fb4984174905240403';
 const apiUrl = 'http://api.weatherapi.com/v1';
@@ -64,10 +19,10 @@ async function fetchWeatherData(city) {
     return null;
   }
 }
-
-async function fetchWeatherForcastData(city) {
+/*
+async function fetchWeatherForecastData(city) {
   try {
-    const response = await fetch(`${apiUrl}/forcast.json?key=${apiKey}&q=${city}&aqi=no`);
+    const response = await fetch(`${apiUrl}/forecast.json?key=${apiKey}&q=${city}&aqi=no`);
     console.log("response: ", response)
     return await response.json();
   } catch (error) {
@@ -75,7 +30,7 @@ async function fetchWeatherForcastData(city) {
     return null;
   }
 }
-
+*/
 
 function App() {
   const [data, setData] = useState(null);
@@ -85,6 +40,12 @@ function App() {
     const weatherData = await fetchWeatherData(city);
     setData(weatherData);
   }
+  /*
+  async function fetchData(city) {
+    const weatherData = await fetchWeatherForecastData(city);
+    setData(weatherData);
+  }
+  */
 
   console.log(data);
 
@@ -93,17 +54,27 @@ function App() {
       <div>
 
         {data && data.current && 
-        (
-          <>
-              <div> {data.location.name} </div>
-              <div>{data.location.region} </div>
-              <div> {data.location.country} </div>
-              <div> {data.location.localtime} </div>
-              <div> {data.current.temp_c} </div>
-              <div>{data.current.temp_f}</div>
-                  </> 
-                )
-              }
+            (
+              <>
+                <div> {data.location.name} </div>
+                <div>{data.location.region} </div>
+                <div> {data.location.country} </div>
+                <div> {data.location.localtime} </div>
+                <div> {data.current.temp_c} </div>
+                <div>{data.current.temp_f}</div>
+              </> 
+            )
+        }
+
+        {data && data.forecast && 
+          (
+            <>
+              <div> {data.forecast.forecastday.day.condition}</div>
+
+            </>
+          )
+        }
+          
         
       </div>
       
@@ -134,40 +105,9 @@ function App() {
 }
 export default App;
 
-//React-router-training 
-/*
-
-import { Link, Route, Routes } from "react-router-dom"
-import {Home } from "./webpages/Home"
-import {InternPage} from "./webpages/InternPage"
-import React, { useState, useEffect } from 'react';
 
 
 
-function App() {
-  return(
-    <>
-    <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/internPage">Intern page</Link></li>
-        </ul>
-    </nav>
 
 
-    
 
-
-  <Routes>
-    <Route path= "/" element={<Home />} />
-    <Route path= "/internPage" element={<InternPage />}/>
-  </Routes>
-  </>
-
-
-  );
-}
-
-export default App;
-
-*/
---
+++ myfetch/src/Practice.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+/*
+    return(
+        <div>
+        <div style={{ width: '100%' }}>
+      <Box
+        sx={{
+          display: 'grid',
+          bgcolor: (theme) =>
+            theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
+          color: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+          border: '1px solid',
+          borderColor: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+          p: 1,
+          borderRadius: 2,
+          fontSize: '0.875rem',
+          fontWeight: '700',
+        }}
+      >
+        {"I'm a grid container!"}
+      </Box>
+    </div>
+
+
+
+
+
+    <div style={{ width: '100%' }}>
+      <Box
+        sx={{
+          display: 'grid',
+          gap: 1,
+          gridTemplateColumns: 'repeat(2, 1fr)',
+        }}
+      >
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </Box>
+    </div>
+    </div>
+    );
+*/
+
+function Practice() {
+    const [data, setData] = useState(null) ;
+    const [city, setCity] = useState("");
+
+
+    return (
+      <div>dlkfjsldkfjsd</div>
+    ) 
+}
+
+export default Practice;
